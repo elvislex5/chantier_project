e9393806d00c991298c9d1b9b80aa1f81af08edc
Elvis <teurtysoyono@gmail.com>
2025-04-14 21:28:14 +0100

    Corrigé n°3

--- lon/lon/settings_ci.py
+++ lon/lon/settings_ci.py
@@ -10,6 +10,15 @@ from .settings_tests import *
 import os
 import dj_database_url
 
+# S'assurer que les applications sont correctement déclarées
+# Particulièrement accounts qui contient notre modèle User personnalisé
+INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'accounts']
+INSTALLED_APPS.append('accounts.apps.AccountsConfig')
+
 # Utiliser l'URL de la base de données des environnements CI
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/lon_test_db')
-DATABASES['default'] = dj_database_url.parse(DATABASE_URL) 
+DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+
+# Dépannage: Forcer le refresh des modèles en cache
+import django.apps
+django.apps.apps.clear_cache() 
