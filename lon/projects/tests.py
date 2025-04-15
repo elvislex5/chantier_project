ea91f24542728d3f3e5b1354ffa06b9fb567414c
Elvis <teurtysoyono@gmail.com>
2025-04-15 15:06:07 +0100

    Corrigé n°11

--- lon/projects/tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-# On évite complètement django
-class BasicTest(unittest.TestCase):
-    """Tests unitaires purs sans aucune dépendance à Django"""
-
-    def test_always_true(self):
-        """Test simple qui passe toujours"""
-        self.assertTrue(True)
